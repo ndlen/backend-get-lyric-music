29b0bce2b7a318a856a19eb5c253a73af7ab121b
Nguyen Duc Len <nguyenduclenqna@gmail.com>
2025-05-18 11:59:25 +0700

    xcs

--- index2.js
+++
@@ -1,9 +0,0 @@
-const { searchByKeyword } = require('nhaccuatui-api-full');
-
-async function getSongIdByName(songName) {
-    const result = await searchByKeyword(songName);
-    console.log(JSON.stringify(result, null, 2));
-}
-
-// Ví dụ sử dụng
-getSongIdByName('Lạc Trôi');
